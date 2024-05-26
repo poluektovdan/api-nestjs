c3321d4151bde0686d474a1daafffdd2b198ef4b
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-05-26 15:36:51 +0300

    Добавлен сервис для отзывов

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,15 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { SuccessResponse } from 'src/helpers/success.response';
-import { DeleteReviewDto } from './dto/delete-review.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { SaveReviewDto } from './dto/save-review.dto';
 import { ReviewModel } from './review.model';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewService } from './review.service';
+import { REVIEW_NOT_FOUND } from './review.constants';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @Post('create')
+  async create(@Body() dto: CreateReviewDto) {
+    return this.reviewService.create(dto);
+  }
+
   @Get('get/:productId')
   async get(@Param('productId') productId: string): Promise<ReviewModel[]> {
-    return null;
+    return this.reviewService.findByProductId(productId);
   }
 
   @Post('save')
@@ -17,8 +34,11 @@ export class ReviewController {
     return null;
   }
 
-  @Delete('delete')
-  async delete(@Body() dto: DeleteReviewDto): Promise<SuccessResponse> {
-    return null;
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const deletedDoc = await this.reviewService.delete(id);
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
   }
 }
